a3cad31ba6c652943458f564c4f3f12084075e79
jkrishnaa25 <144918058+jkrishnaa25@users.noreply.github.com>
2026-07-14 12:40:09 +0530

    Update alu.v

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,9 +1,11 @@
-module alu(a,b,opcode,result);
+module alu(a,b,opcode,result,status_reg);
 
   input [7:0]a;
   input [7:0]b; 
   input [2:0]opcode;
   output reg [7:0]result;
+  output reg [3:0] status_reg;
+  reg [8:0]temp;
   
   parameter ADD = 3'b000,
     		SUB = 3'b001,
@@ -12,44 +14,95 @@ module alu(a,b,opcode,result);
     		XOR = 3'b100,
     		CMP = 3'b101,
     		NOT = 3'b110,
-    		NOR = 3'b111;
+    		NOP = 3'b111;
   
   always @(*) begin
     case(opcode)
       ADD : 
         begin
-          result = a + b;
+          temp = a + b;
+          result = temp[7:0];
+          status_reg[3] = temp[8]; 								 //carry
+          status_reg[2] = (result == 8'd0); 					 //zero
+          status_reg[1] = (~(a[7] ^ b[7])) & (a[7] ^ result[7]); //overflow
+          status_reg[0] = result[7];                             //negative
         end
       SUB : 
         begin
-		  result = a - b;
+		  temp = a - b;
+          result = temp[7:0];
+          status_reg[3] = temp[8];
+          status_reg[2] = (result == 8'd0);
+          status_reg[1] = ((a[7] ^ b[7])) & (a[7] ^ result[7]);
+          status_reg[0] = result[7];
         end
       AND : 
         begin
-          result = a & b;
+          temp = a & b;
+          result = temp[7:0];
+          status_reg[3] = 0;
+          status_reg[2] = (result == 8'd0);
+          status_reg[1] = 0;
+          status_reg[0] = result[7];
+
         end
       OR : 
         begin
-          result = a | b;
+          temp = a | b;
+          result = temp[7:0];
+          status_reg[3] = 0;
+          status_reg[2] = (result == 8'd0);
+          status_reg[1] = 0;
+          status_reg[0] = result[7];
+
         end
       XOR : 
         begin
-          result = a ^ b;
+          temp = a ^ b;
+          result = temp[7:0];
+          status_reg[3] = 0;
+          status_reg[2] = (result == 8'd0);
+          status_reg[1] = 0;
+          status_reg[0] = result[7];
+
         end
       CMP : 
         begin
-          result = a - b;
+          temp = a - b;
+          result = temp[7:0];
+          status_reg[3] = temp[8];
+          status_reg[2] = (result == 8'd0);
+          status_reg[1] = ((a[7] ^ b[7])) & (a[7] ^ result[7]);
+          status_reg[0] = result[7];
+
         end
       NOT : 
         begin
-          result = ~a;
+          temp = ~a;
+          result = temp[7:0];
+          status_reg[3] = 0;
+          status_reg[2] = (result == 8'd0);
+          status_reg[1] = 0;
+          status_reg[0] = result[7];
         end
-      NOR : 
+      NOP : 
         begin
-          result = ~(a | b);
+          temp = 8'd0;
+          result = temp[7:0];
+          status_reg[3] = 0;
+          status_reg[2] = (result == 8'd0);
+          status_reg[1] = 0;
+          status_reg[0] = result[7];
         end
-      default:
-    	result = 8'd0;
+      default: begin
+        temp = 8'd0;
+        result = temp[7:0];
+        status_reg[3] = 0;
+        status_reg[2] = (result == 8'd0);
+        status_reg[1] = 0;
+        status_reg[0] = result[7];
+      end
+
     endcase
   end
 endmodule
